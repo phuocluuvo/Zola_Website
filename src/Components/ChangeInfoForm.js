13e4abee35a38ab8d3e280bc39e5e2fa91b2f26c
volincoln <voluu0702@gmail.com>
2022-11-28 13:00:53 +0700

    fix ui change info

--- src/Components/ChangeInfoForm.js
+++ src/Components/ChangeInfoForm.js
@@ -165,133 +165,135 @@ function ChangeInfoForm({ userUpdate }) {
             <Tab>Change Password</Tab>
           </TabList>
           <TabPanels>
-            <Formik
-              initialValues={{
-                _id: user._id,
-                fullname: user.fullname,
-                username: user.username,
-                pic: user.pic,
-              }}
-              onSubmit={(values, actions) => {
-                handleSubmitChangeInfo(values);
-                actions.setSubmitting(false);
-              }}
-            >
-              {(props) => (
-                <Form className="flex">
-                  <Box w="full" flex={0.3}>
-                    <Field name="pic">
-                      {({ field, form }) => (
-                        <FormControl className="justify-center flex flex-col items-center w-full">
-                          <Input
-                            id="newAvatar"
-                            type="file"
-                            hidden
-                            accept="image/*"
-                            p="1.5"
-                            onChange={(e) => postDetails(e.target.files[0])}
-                          />
+            <TabPanel>
+              <Formik
+                initialValues={{
+                  _id: user._id,
+                  fullname: user.fullname,
+                  username: user.username,
+                  pic: user.pic,
+                }}
+                onSubmit={(values, actions) => {
+                  handleSubmitChangeInfo(values);
+                  actions.setSubmitting(false);
+                }}
+              >
+                {(props) => (
+                  <Form className="flex">
+                    <Box w="full" flex={0.3}>
+                      <Field name="pic">
+                        {({ field, form }) => (
+                          <FormControl className="justify-center flex flex-col items-center w-full">
+                            <Input
+                              id="newAvatar"
+                              type="file"
+                              hidden
+                              accept="image/*"
+                              p="1.5"
+                              onChange={(e) => postDetails(e.target.files[0])}
+                            />
 
-                          <label htmlFor="newAvatar">
-                            <Tooltip label="Change Avatar">
-                              <Box pos="relative" cursor={"pointer"}>
-                                <Avatar
-                                  name={user.fullname}
-                                  src={pic}
-                                  size="xl"
-                                />
-                                <Box
-                                  _hover={{
-                                    border: "2px solid white",
-                                    rounded: "full",
-                                    bgColor: "whiteAlpha.500",
-                                    opacity: "1",
-                                  }}
-                                  opacity="0"
-                                  w="full"
-                                  h="full"
-                                  className="absolute z-10 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 transition-all"
-                                >
-                                  <AiFillEdit
-                                    fontSize={"40"}
-                                    color="white"
-                                    className="absolute z-10 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 "
+                            <label htmlFor="newAvatar">
+                              <Tooltip label="Change Avatar">
+                                <Box pos="relative" cursor={"pointer"}>
+                                  <Avatar
+                                    name={user.fullname}
+                                    src={pic}
+                                    size="xl"
                                   />
+                                  <Box
+                                    _hover={{
+                                      border: "2px solid white",
+                                      rounded: "full",
+                                      bgColor: "whiteAlpha.500",
+                                      opacity: "1",
+                                    }}
+                                    opacity="0"
+                                    w="full"
+                                    h="full"
+                                    className="absolute z-10 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 transition-all"
+                                  >
+                                    <AiFillEdit
+                                      fontSize={"40"}
+                                      color="white"
+                                      className="absolute z-10 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 "
+                                    />
+                                  </Box>
                                 </Box>
-                              </Box>
-                            </Tooltip>
-                          </label>
-                        </FormControl>
-                      )}
-                    </Field>
-                  </Box>
-                  <VStack w="full" flex={0.7} className="mx-2">
-                    <Field
-                      name="username"
-                      validate={validateUserName}
-                      className="w-full"
-                    >
-                      {({ field, form }) => (
-                        <FormControl
-                          isInvalid={
-                            form.errors.username && form.touched.username
-                          }
-                        >
-                          <InputGroup>
-                            <InputLeftElement>
-                              <Text fontWeight={"bold"}>@</Text>
-                            </InputLeftElement>
-                            <Input
-                              {...field}
-                              isInvalid={form.errors.username === null}
-                              rounded={"lg"}
-                              placeholder={"Enter your user name"}
-                            />
-                          </InputGroup>
-                          <FormErrorMessage>
-                            {form.errors.username}
-                          </FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                    <Field
-                      name="fullname"
-                      validate={validateFullName}
-                      className="w-full"
-                    >
-                      {({ field, form }) => (
-                        <FormControl>
-                          <InputGroup>
-                            <InputLeftElement>
-                              <Text fontWeight={"bold"}>n</Text>
-                            </InputLeftElement>
-                            <Input
-                              {...field}
-                              isInvalid={form.errors.fullname === null}
-                              rounded={"lg"}
-                              placeholder={"Enter your new full name"}
-                            />
-                          </InputGroup>
-                          <FormErrorMessage>
-                            {form.errors.fullname}
-                          </FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                    <Box display="flex" w="full">
-                      <Spacer />
-                      <Button
-                        colorScheme="teal"
-                        isLoading={props.isSubmitting}
-                        type="submit"
-                      >
-                        Apply
-                      </Button>
+                              </Tooltip>
+                            </label>
+                          </FormControl>
+                        )}
+                      </Field>
                     </Box>
-                  </VStack>
-                </Form>
-              )}
-            </Formik>
+                    <VStack w="full" flex={0.7} className="mx-2">
+                      <Field
+                        name="username"
+                        validate={validateUserName}
+                        className="w-full"
+                      >
+                        {({ field, form }) => (
+                          <FormControl
+                            isInvalid={
+                              form.errors.username && form.touched.username
+                            }
+                          >
+                            <InputGroup>
+                              <InputLeftElement>
+                                <Text fontWeight={"bold"}>@</Text>
+                              </InputLeftElement>
+                              <Input
+                                {...field}
+                                isInvalid={form.errors.username === null}
+                                rounded={"lg"}
+                                placeholder={"Enter your user name"}
+                              />
+                            </InputGroup>
+                            <FormErrorMessage>
+                              {form.errors.username}
+                            </FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                      <Field
+                        name="fullname"
+                        validate={validateFullName}
+                        className="w-full"
+                      >
+                        {({ field, form }) => (
+                          <FormControl>
+                            <InputGroup>
+                              <InputLeftElement>
+                                <Text fontWeight={"bold"}>n</Text>
+                              </InputLeftElement>
+                              <Input
+                                {...field}
+                                isInvalid={form.errors.fullname === null}
+                                rounded={"lg"}
+                                placeholder={"Enter your new full name"}
+                              />
+                            </InputGroup>
+                            <FormErrorMessage>
+                              {form.errors.fullname}
+                            </FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                      <Box display="flex" w="full">
+                        <Spacer />
+                        <Button
+                          colorScheme="teal"
+                          isLoading={props.isSubmitting}
+                          type="submit"
+                        >
+                          Apply
+                        </Button>
+                      </Box>
+                    </VStack>
+                  </Form>
+                )}
+              </Formik>
+            </TabPanel>
             <TabPanel>
               <VStack>
                 <Input
